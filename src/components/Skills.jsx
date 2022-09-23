3c7068ce2f243ac5da7bf8f65333799374805c8c
emilisz <eciurlionis@gmail.com>
2022-09-23 19:31:20 +0300

    added gallery2

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,24 +9,24 @@ const Skills = () => {
       <div className="my-3 py-6 px-3 border-y border-slate-500 mt-6">
         <p className="py-3 text-left ml-3  border-gray-900 border-b-0 text-lg font-bold">Proficient with:</p>
         <div className="grid grid-cols-3 lg:ml-3 border border-gray-900 rounded-t-3xl divide-x divide-gray-900 bg-gray-800 bg-blend-darken">
-          <div className="p-6 lg:p-3 w-full">
+          <div className="p-6 lg:p-3 w-full hover:bg-gray-700 rounded-tl-3xl" title="HTML">
             <img className="bg-cover bg-center w-full" src="../img/html.png" alt="" />
           </div>
-          <div className="p-6 lg:p-3 w-full">
+          <div className="p-6 lg:p-3 w-full hover:bg-gray-700" title="Laravel">
             <img className="bg-cover bg-center w-full" src="../img/laravel.png" alt="" />
           </div>
-          <div className="p-6 lg:p-3 w-full">
+          <div className="p-6 lg:p-3 w-full hover:bg-gray-700 rounded-tr-3xl" title="Tailwindcss">
             <img className="bg-cover bg-center w-full" src="../img/tailwind.png" alt="" />
           </div>
         </div>
         <div className="grid grid-cols-3 lg:ml-3 border border-gray-900 border-t-0 rounded-b-3xl divide-x divide-gray-900 bg-gray-800 bg-blend-darken">
-          <div className="p-6 lg:p-3 w-full">
+          <div className="p-6 lg:p-3 w-full hover:bg-gray-700 rounded-bl-3xl" title="React">
             <img className="bg-cover bg-center w-full" src="../img/react.png" alt="" />
           </div>
-          <div className="p-6 lg:p-3 w-full">
+          <div className="p-6 lg:p-3 w-full hover:bg-gray-700" title="Mysql">
             <img className="bg-cover bg-center w-full" src="../img/mysql.png" alt="" />
           </div>
-          <div className="p-6 lg:p-3 w-full">
+          <div className="p-6 lg:p-3 w-full hover:bg-gray-700 rounded-br-3xl" title="Bootstrap">
             <img className="bg-cover bg-center w-full" src="../img/bootstrap.png" alt="" />
           </div>
         </div>
--- src/components/partials/Gallery.jsx
+++ src/components/partials/Gallery.jsx
@@ -1,18 +1,17 @@
-import { Carousel } from 'flowbite-react'
-import React from 'react'
-import ExperienceButtons from './ExperienceButtons'
+import { Carousel } from "flowbite-react";
+import React, { useState } from "react";
+import ExperienceButtons from "./ExperienceButtons";
 
 const Gallery = () => {
+
   return (
     <>
-         <div className="flex flex-col md:flex-row gap-2 py-3 border-t border-slate-500 px-2 lg:pl-3 lg:pr-6 hover:bg-gray-800 hover:bg-blend-darken ">
+      <div className="flex flex-col md:flex-row gap-2 py-3 border-t border-slate-500 px-2 lg:pl-3 lg:pr-6 hover:bg-gray-800 hover:bg-blend-darken ">
         <div className="flex flex-row gap-2 lg:w-20">
-        <img className="rounded-full w-16 h-16" src="../img/freelancer.png" alt="" />
-        <div className="md:hidden mt-2">
+          <img className="rounded-full w-16 h-16" src="../img/freelancer.png" alt="" />
+          <div className="md:hidden mt-2">
             <div className=" flex flex-row ">
-              <h3 className="font-bold">
-                 Coded web design
-              </h3>
+              <h3 className="font-bold">Coded web design</h3>
             </div>
             <p className=" text-gray-400"></p>
           </div>
@@ -20,39 +19,24 @@ const Gallery = () => {
         <div className="flex flex-col  w-full gap-2 mt-3 px-3 lg:mx-3 ">
           <div className="hidden md:block">
             <div className=" flex flex-row ">
-              <h3 className="font-bold">
-              Coded web design
-              </h3>
+              <h3 className="font-bold">Coded web design</h3>
             </div>
             <p className=" text-gray-400"></p>
           </div>
 
           <div className="h-56 sm:h-96 xl:h-96 2xl:h-96">
-  <Carousel slideInterval={5000}>
-    <img
-      src="../img/other1.webp"
-      alt="..."
-    />
-    <img
-      src="../img/other2.webp"
-      alt="..."
-    />
-    <img
-      src="../img/other3.webp"
-      alt="..."
-    />
-    <img
-      src="../img/other4.webp"
-      alt="..."
-    />
-  </Carousel>
-</div>
-
+            <Carousel slideInterval={5000}>
+              <img src="../img/other1.webp" alt="..." />
+              <img src="../img/other2.webp" alt="..." />
+              <img src="../img/other3.webp" alt="..." />
+              <img src="../img/other4.webp" alt="..." />
+            </Carousel>
+          </div>
           <ExperienceButtons />
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
