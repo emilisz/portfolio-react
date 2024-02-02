0da4dce2d3bee4bbac925a624fed8390e9944281
emilisz <eciurlionis@gmail.com>
2024-02-02 13:09:57 +0200

    website

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -100,6 +100,25 @@ const Contacts = () => {
         </div>
       </div>
 
+
+      <div className="flex gap-3 py-3 my-3 px-2 rounded-full hover:bg-gray-800">
+        {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+        </svg> */}
+
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-blue-500">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
+</svg>
+
+
+        <div className="flex flex-col text-left -mt-2 ">
+          <h2 className="font-semibold">Website:</h2>
+          <a className="-mt-1 text-gray-400" href="https://emilis-cv.vercel.app" target="_blank" rel="noopener noreferrer">
+          https://emilis-cv.vercel.app
+          </a>
+        </div>
+      </div>
+
       <div className="flex gap-3 py-3 my-3 px-2 rounded-full">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path
